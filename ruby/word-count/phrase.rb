b84e937cfc4d0681bf4744e535cae581822cb6d8
Ryan Mes <ryan.mes@getsmarter.co.za>
2015-02-12 13:36:09 +0200

    Refactored regex a bit more after doing a bit of revision ;)

--- ruby/word-count/phrase.rb
+++ ruby/word-count/phrase.rb
@@ -1,15 +1,19 @@
 class Phrase
-  def initialize value
-    @value = value
+  attr_reader :text
+
+  def initialize text
+    @text = text.downcase
+    @word_count = Hash.new(0)
   end
 
   def word_count
-    value_grouped_by_word.keys.each { |key| w[key] = w[key].count }
+    words.each { |word| @word_count[word] += 1 } unless @word_count.any?
+    @word_count
   end
 
   private
 
-  def value_grouped_by_word
-    @value_grouped_by_word ||= value.split(/\W+/).group_by{ |word| word }
+  def words
+    @words ||= text.scan(/\w+\'\w+|\w+/)
   end
 end
