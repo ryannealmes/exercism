bbfb2abf29eb85602128030de94d8bc90ffe445c
Ryan-Neal Mes <ryan.mes@gmail.com>
2015-07-04 20:24:01 +0200

    Minor performance improvement

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,12 +1,12 @@
 class Sieve
-  attr_reader :numbers
+  attr_reader :numbers, :primes
 
   def initialize value
     @numbers = (2..value).to_a
   end
 
   def primes
-    numbers.each do |n|
+    @primes ||= numbers.each do |n|
       numbers.delete_if {|i| i > n && i % n == 0 }
     end
   end
